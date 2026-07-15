7132aa6fc86bf90773da6524c821af54ac919b0d
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2026-07-15 08:15:11 -0600

    adding filter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivateChild: [authGuard],
+    children: [],
+  },
+];
